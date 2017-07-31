c327d90648571f6ef4309cafc6d3c70963dc583c
Oscar Cauich <ocauich@gravitypayments.com>
2017-07-31 15:51:53 -0700

    added the redux thunk middleware

--- lab-oscar/frontend/src/component/app/index.js
+++ lab-oscar/frontend/src/component/app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {provider} from 'react-redux';
 import {BrowserRouter, Route} from 'react-router-dom';
-// import appStoreCreate from '../../lib/app-store-create.js';
+import appStoreCreate from '../../lib/app-store-create.js';
 import Dashboard from '../dashboard';
 
 // const store = appStoreCreate();
---
+++ lab-oscar/frontend/src/lib/redux-thunk.js
@@ -0,0 +1,8 @@
+// enables us to make async action creators
+//which will in turn give us the ability to
+//make action creators that make ajax requrests
+
+export default store => next => action =>
+  typeof action === 'function'
+    ? action(store.dispatch, store.getState)
+    : next(action);
